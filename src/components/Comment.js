52d4815c8233d3a2d0135f17ef8cb68fc4af504b
aamir <aamir6582@gmail.com>
2024-11-03 16:28:10 +0530

    context used in Comment Component

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { useContext } from "react";
+import UserInfoContext from "../context/UserInfoContext";
 
 export default function Comment() {
+  const { username, isAdmin } = useContext(UserInfoContext);
   return (
     <div>
-      <p>Logged in as ...</p>
+      <p>Logged in as {username}</p>
+      {isAdmin && <button>Edit comment</button>}
     </div>
   );
 }
