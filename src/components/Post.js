58745dadc69b7a4e026e7b923fd782de974bc587
aamir <aamir6582@gmail.com>
2024-11-03 16:31:48 +0530

    context used in Post Component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Comment from "./Comment";
+import { useContext } from "react";
+import UserInfoContext from "../context/UserInfoContext";
 
 export default function Post() {
+  const { isAdmin } = useContext(UserInfoContext);
   return (
     <div>
+      {isAdmin && <button>Delete</button>}
       <h2>Example Post Title</h2>
       <p>This is an example post content</p>
       <Comment></Comment>
